50225debab0e39970f002abd5b3ce0bc2d040d60
NicolasAssunção <100379835+projetonicolas@users.noreply.github.com>
2023-03-15 15:11:37 -0300

    Add files via upload

---
+++ Exercicio AlunoMedia/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ExerciciosAluno;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        Aluno aluno1 =new Aluno();
+        aluno1.nome= "Fulano da Silva";
+        aluno1.nota1=4.3;
+        aluno1.nota2=4;
+
+        aluno1.mensagem();
+
+    }
+}
