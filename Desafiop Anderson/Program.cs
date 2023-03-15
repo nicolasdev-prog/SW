6cdd6cf4b4298e39e846ec76d5826b316e723371
NicolasAssunção <100379835+projetonicolas@users.noreply.github.com>
2023-03-15 16:37:18 -0300

    Add files via upload

---
+++ Desafiop Anderson/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DesafioAnderson;
+class Program
+{
+        static void Main(string[] args)
+    {
+        Aluno aluno1 = new Aluno();
+
+
+        Console.WriteLine("Introduza a primeira nota: ");
+        double nota1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("primeira nota é: " + nota1);
+
+
+        Console.WriteLine("Introduza a segunda nota: ");
+        double nota2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("segunda nota é: " + nota2);
+
+
+        Console.WriteLine("Insira o nome do aluno: ");
+        string? nome = Console.ReadLine();
+        Console.WriteLine("Qual é o nome " + nome);
+
+
+
+        aluno1.nome = nome;
+        aluno1.nota1 = nota1;
+        aluno1.nota2 = nota2;
+
+        aluno1.mensagem();
+    }
+}
