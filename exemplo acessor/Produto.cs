f95ae935cc934cd1113ee09f90012d12d21d5571
NicolasAssunção <100379835+projetonicolas@users.noreply.github.com>
2023-03-29 16:52:11 -0300

    Add files via upload

---
+++ exemplo acessor/Produto.cs
@@ -0,0 +1,24 @@
+namespace exemplo_acessor;
+class Produto
+{
+public string? Nome{get;set;}
+
+public double Preco{get;set;}
+
+public void MostraDados(){
+   Console.WriteLine("Nome:" + this.Nome);
+   Console.WriteLine("Preco:" + this.Preco);
+}
+
+public void AlteraPreco(double preco){
+    if(preco >=0){
+    this.Preco = preco;
+}else{
+    Console.WriteLine("VALOR INVÁLIDO!");
+}
+
+}
+public void AlteraNome(string nome){
+    this.Nome=nome;
+}
+}
---
+++ exemplo acessor/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace exemplo_acessor;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto p1 = new Produto();
+        //p1.Preco = 20;
+        p1.AlteraPreco(15);
+        p1.AlteraNome("PARAFUSO");
+        p1.MostraDados();
+        //Console.Writeline("Gaveta: " +p1.Preco);
+    }
+}
